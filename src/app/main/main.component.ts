69648cc93d0c03c678a20b6d3c68a53848cb4ee4
Shasi-vns <shashidharshashi52@gmail.com>
2023-07-28 18:07:58 +0530

    just commit after using pritter

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,114 +8,144 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-
   public employee: Employee[] = [];
-  public editEmployee:Employee | any;
-  public deleteEmployee:Employee | any;
-  public userName : string | any;
-  public p:number=1;
-  public itemsPerPage=8;
-  public totalCards:any;
-  
-  constructor (private toastr: ToastrService,private employeeService: EmployeeService, private userAuthSer:UserAuthService){}
+  public editEmployee: Employee | any;
+  public deleteEmployee: Employee | any;
+  public userName: string | any;
+  public p: number = 1;
+  public itemsPerPage = 8;
+  public totalCards: any;
+
+  constructor(
+    private toastr: ToastrService,
+    private employeeService: EmployeeService,
+    private userAuthSer: UserAuthService
+  ) {}
 
   ngOnInit(): void {
     this.getEmployees();
     this.isLoggedIn();
-    this.userName=this.userAuthSer.getName();
+    this.userName = this.userAuthSer.getName();
   }
 
-  public isLoggedIn(){
+  public isLoggedIn() {
     return this.userAuthSer.isLoggedIn();
   }
 
-
-  public getEmployees():void{
-    this.employeeService.getEmployees().subscribe(
-     { next : (response: Employee[])=> {this.employee=response,this.totalCards=response.length,console.log(response.values)},
-      error : (error:HttpErrorResponse)=> alert(error.message),
-      complete:() => console.log("heello")
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (response: Employee[]) => {
+        (this.employee = response),
+          (this.totalCards = response.length),
+          console.log(response.values);
+      },
+      error: (error: HttpErrorResponse) => alert(error.message),
+      complete: () => console.log('heello'),
     });
   }
 
-  public onAddEmployee(addForm: NgForm):void{
+  public onAddEmployee(addForm: NgForm): void {
     document.getElementById('add-employee-form')?.click();
-    this.employeeService.addEmployee(addForm.value).subscribe(
-      { next : (response:Employee)=> {this.toastr.success('Employee added successfully','Record Added',{timeOut: 1500}),this.getEmployees()},
-       error :(error:HttpErrorResponse)=>this.toastr.error(error.message,'Record Additon Failed',{timeOut: 1500}),
-      complete:()=>addForm.reset()
-  });
+    this.employeeService.addEmployee(addForm.value).subscribe({
+      next: (response: Employee) => {
+        this.toastr.success('Employee added successfully', 'Record Added', {
+          timeOut: 1500,
+        }),
+          this.getEmployees();
+      },
+      error: (error: HttpErrorResponse) =>
+        this.toastr.error(error.message, 'Record Additon Failed', {
+          timeOut: 1500,
+        }),
+      complete: () => addForm.reset(),
+    });
   }
 
-  public onUpdateEmployee(employee: Employee):void{
-    this.employeeService.updateEmployee(employee).subscribe(
-      {next: (response:Employee)=>{this.toastr.info('Employee updated successfully','Record Update',{timeOut: 1500}),this.getEmployees()},
-      error : (error:HttpErrorResponse)=>this.toastr.error(error.message,'Record Update Failed',{timeOut: 1500}),
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe({
+      next: (response: Employee) => {
+        this.toastr.info('Employee updated successfully', 'Record Update', {
+          timeOut: 1500,
+        }),
+          this.getEmployees();
+      },
+      error: (error: HttpErrorResponse) =>
+        this.toastr.error(error.message, 'Record Update Failed', {
+          timeOut: 1500,
+        }),
       // complete:()=>console.log("")
-      });
+    });
   }
 
-  public onDeleteEmployee(employeeId: number):void{
-   
-    this.employeeService.deleteEmployee(employeeId).subscribe(
-     {next: (response:void)=> {this.toastr.success('Employee Deleted successfully','Record Delete',{timeOut: 1500}),this.getEmployees()},
-      error: (error:HttpErrorResponse)=>this.toastr.error(error.message,'Record Deletion Failed',{timeOut: 1500}),
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe({
+      next: (response: void) => {
+        this.toastr.success('Employee Deleted successfully', 'Record Delete', {
+          timeOut: 1500,
+        }),
+          this.getEmployees();
+      },
+      error: (error: HttpErrorResponse) =>
+        this.toastr.error(error.message, 'Record Deletion Failed', {
+          timeOut: 1500,
+        }),
       // complete:()=>console.log("")
-  });
+    });
   }
 
-  public searchEmployees(key:string):void{
-    const result:Employee[]=[];
+  public searchEmployees(key: string): void {
+    const result: Employee[] = [];
     console.log(key);
-    for (const employee of this.employee){
-      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      ||employee.department.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      ||employee.id.toString().indexOf(key) !== -1
+    for (const employee of this.employee) {
+      if (
+        employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.department.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.id.toString().indexOf(key) !== -1
       ) {
         result.push(employee);
       }
     }
-    this.employee=result;
-    if (result.length === 0 || !key){
+    this.employee = result;
+    if (result.length === 0 || !key) {
       this.getEmployees();
     }
   }
 
-
-public onOpenModal(employee:Employee | any,mode:any):void{
-  const container = document.getElementById('main-container');
-  const button = document.createElement('button');
-  button.type='button';
-  button.style.display='none';
-  button.setAttribute('data-toggle','modal');
-  if(mode === 'add'){
-    button.setAttribute('data-target','#addEmployeeModal');
-  }
-  if(mode === 'edit'){
-    if(this.userAuthSer.getRole() === "ADMIN"){
-    this.editEmployee=employee;
-    button.setAttribute('data-target','#updateEmployeeModal');
+  public onOpenModal(employee: Employee | any, mode: any): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
     }
-    else{
-        this.toastr.warning('You Dont have access','Updation',{timeOut: 1500})
+    if (mode === 'edit') {
+      if (this.userAuthSer.getRole() === 'ADMIN') {
+        this.editEmployee = employee;
+        button.setAttribute('data-target', '#updateEmployeeModal');
+      } else {
+        this.toastr.warning('You Dont have access', 'Updation', {
+          timeOut: 1500,
+        });
+      }
     }
-  }
-  if(mode === 'delete'){
-    if(this.userAuthSer.getRole() === "ADMIN"){
-    this.deleteEmployee=employee;
-    button.setAttribute('data-target','#deleteEmployeeModal');
-  }
-  else{
-    this.toastr.warning('You Dont have access','Deletion',{timeOut: 1500})
-  }
-}
+    if (mode === 'delete') {
+      if (this.userAuthSer.getRole() === 'ADMIN') {
+        this.deleteEmployee = employee;
+        button.setAttribute('data-target', '#deleteEmployeeModal');
+      } else {
+        this.toastr.warning('You Dont have access', 'Deletion', {
+          timeOut: 1500,
+        });
+      }
+    }
 
-container?.appendChild(button);
-button.click();
-}
-
+    container?.appendChild(button);
+    button.click();
+  }
 }
